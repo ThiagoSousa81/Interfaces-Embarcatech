f72695497b2878e672c9d38ef9f608c22aca5ea1
Thiago Sousa <thiagosousa81@gmail.com>
2025-02-05 23:51:33 -0300

    § debounce e botões §

--- Interfaces-Embarcatech.c
+++ Interfaces-Embarcatech.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "hardware/uart.h"
@@ -19,11 +20,74 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
+#define LED_PIN_G 11   // Pino do verde do LED RGB
+#define LED_PIN_B 12   // Pino do azul do LED RGB
+const uint BUTTON_A = 5; // Pino GPIO do botão A
+const uint BUTTON_B = 6; // Pino GPIO do botão B
+
+// Armazena o tempo do último click de botão (em microssegundos)
+// O tipo volatile é para a variável ser acessada em modo assíncrono
+static volatile uint32_t last_time = 0;
+static volatile bool led_state_G = false, led_state_B = false;
+
+// Função de interrupção IRQ com debouncing dos botões A e B
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    // Obtém o tempo atual em microssegundos
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+    // Verifica se passou tempo suficiente desde o último evento
+    if (current_time - last_time > 300000) // 300 ms de debouncing
+    {        
+        if (gpio == BUTTON_A)
+        {
+            led_state_G = !led_state_G;
+            gpio_put(LED_PIN_G, led_state_G);
+
+            printf("Estado do LED verde alterado!");
+            // Enviar o que acontece para o display
+            
+            last_time = current_time; // Atualiza o tempo do último evento
+        }
+        else
+        {
+            led_state_B = !led_state_B;
+            gpio_put(LED_PIN_B, led_state_B);
+
+            printf("Estado do LED azul alterado!");
+            // Enviar o que acontece para o display
+            
+            last_time = current_time; // Atualiza o tempo do último evento
+        }
+    }
+}
 
 
 int main()
 {
-    stdio_init_all();
+    stdio_init_all(); // Inicializar a comunicação serial
+
+    // Inicializar o pino GPIO13
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, true);
+
+    gpio_init(LED_PIN_B);
+    gpio_set_dir(LED_PIN_B, true);
+
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN); // Configura o pino como entrada
+    gpio_pull_up(BUTTON_A);          // Habilita o pull-up interno
+    // Configuração da interrupção com callback do botão A
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);    
+
+    gpio_init(BUTTON_B);
+    gpio_set_dir(BUTTON_B, GPIO_IN); // Configura o pino como entrada
+    gpio_pull_up(BUTTON_B);          // Habilita o pull-up interno
+    // Configuração da interrupção com callback do botão B
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+
+
+
 
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
@@ -50,7 +114,7 @@ int main()
     // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
 
     while (true) {
-        uart_puts(UART_ID, " Hello, UART!\n");
-        sleep_ms(1000);
+        //uart_puts(UART_ID, " Hello, UART!\n");
+        //sleep_ms(1000);
     }
 }
