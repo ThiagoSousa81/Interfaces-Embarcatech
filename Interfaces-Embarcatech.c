dfbf7963667d3d73b8c77ca7b3cccbaddde9d7b6
Thiago Sousa <thiagosousa81@gmail.com>
2025-02-06 00:13:50 -0300

    § funcional display §

--- Interfaces-Embarcatech.c
+++ Interfaces-Embarcatech.c
@@ -2,6 +2,9 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "hardware/uart.h"
+// Bibliotecas para o display
+#include "inc/ssd1306.h"
+#include "inc/font.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -9,6 +12,7 @@
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
+#define endereco 0x3C
 
 // UART defines
 // By default the stdout UART is `uart0`, so we will use the second one
@@ -92,11 +96,22 @@ int main()
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
     
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA); // Pull up the data line
+    gpio_pull_up(I2C_SCL); // Pull up the clock line
+
+    ssd1306_t ssd; // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+
 
     // Set up our UART
     uart_init(UART_ID, BAUD_RATE);
@@ -113,8 +128,15 @@ int main()
     
     // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
 
+    bool cor = true;
     while (true) {
-        //uart_puts(UART_ID, " Hello, UART!\n");
-        //sleep_ms(1000);
+        cor = !cor;
+        // Atualiza o conteúdo do display com animações
+        ssd1306_fill(&ssd, !cor); // Limpa o display
+        ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
+        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
+        ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48); // Desenha uma string      
+        ssd1306_send_data(&ssd); // Atualiza o display
     }
 }
